a6fb9cf321561b06ec5f071a98bda08eac72fb6b
Michael Bestas <mkbestas@lineageos.org>
2018-09-17 22:43:53 +0300

    msm8974-common: Switch to 1.1 power HAL
    Change-Id: I4690a5b913141427a828d6a3a4af407d35a10775

--- msm8974-common.mk
+++ msm8974-common.mk
@@ -191,7 +191,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-service-qti \
+    android.hardware.power@1.1-service-qti \
     libshim_atomic
 
 # Proprietery Firmware
