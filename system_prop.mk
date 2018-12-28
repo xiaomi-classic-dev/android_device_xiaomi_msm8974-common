LineageOS/android_device_htc_msm8974-common

msm8974-common: Update prefixes for audio system properties
 * All vendor-specific properties are required to have vendor prefixes

Change-Id: I562639e93b8eca4914473575c13da958224181f8

system_prop.mk
@@ -5,18 +5,13 @@
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
     af.fast_track_multiplier=1 \
-    audio_hal.period_size=192 \
     av.offload.enable=false \
-    tunnel.audio.encode=true \
-    use.voice.path.for.pcm.voip=false
-
-# fluencetype can be "fluence" or "fluencepro" or "none"
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.qc.sdk.audio.fluencetype=fluence
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.audio.fluence.voicecall=true \
-    persist.audio.fluence.speaker=false
+    persist.vendor.audio.fluence.voicecall=true \
+    persist.vendor.audio.fluence.speaker=false \
+    ro.vendor.audio.sdk.fluencetype=fluence \
+    vendor.audio_hal.period_size=192 \
+    vendor.audio.tunnel.encode=true \
+    vendor.voice.path.for.pcm.voip=false
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \

xiaomi-classic-dev/android_device_xiaomi_msm8974-common

system_prop.mk
@@ -54,13 +54,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.qc.sdk.camera.facialproc=false
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.qc.sdk.audio.ssr=false \
-    ro.qc.sdk.audio.fluencetype=fluence \
-    use.voice.path.for.pcm.voip=true \
-    persist.audio.fluence.voicecall=true \
-    persist.audio.fluence.voicerec=false \
+    ro.vendor.audio.sdk.ssr=false \
+    ro.vendor.audio.sdk.fluencetype=fluence \
+    vendor.voice.path.for.pcm.voip=true \
+    persist.vendor.audio.fluence.voicecall=true \
+    persist.vendor.audio.fluence.voicerec=false \
     af.fast_track_multiplier=1 \
-    audio_hal.period_size=192 \
+    vendor.audio_hal.period_size=192 \
     persist.audio.vns.mode=2
 
 # Enalbe modem fast dormancy function