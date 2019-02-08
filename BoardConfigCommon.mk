553bb878186e81c1c283356164d864765939d6f7
Anton Hansson <hansson@google.com>
2019-02-08 17:56:34 +0000

    msm8974-common: Use generic arch variant
    Follow blueline which has not demonstrated any regressions
    after switching to a generic arch variant.
    
    Bug: 120773446
    Test: make
    Change-Id: I9e23963fcfa5ccaa912f2930a632458c4d2a692d

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -37,7 +37,7 @@ TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI      := armeabi-v7a
 TARGET_CPU_ABI2     := armeabi
 TARGET_CPU_SMP      := true
-TARGET_CPU_VARIANT  := krait
+TARGET_CPU_VARIANT  := generic
 
 # ANT+
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
