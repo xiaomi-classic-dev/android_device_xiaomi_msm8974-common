LineageOS/android_device_htc_msm8974-common

msm8974-common: Enable GL comp backpressure
Enable backpressure for client composition to avoid jank due to
HWC queue stuffing.

Test: 1. Open Camera
      2. Swipe horizontally to go back to previous app.

Bug: 138083790
Change-Id: I3d26aa470c1660b23bd97133f39858f92638aca8

system_prop.mk
@@ -35,6 +35,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.egl.hw=1 \
     debug.hwui.use_buffer_age=false \
     debug.mdpcomp.logs=0 \
+    debug.sf.enable_gl_backpressure=1 \
     debug.sf.hw=1 \
     dev.pm.dyn_samplingrate=1 \
     persist.hwc.mdpcomp.enable=true \

xiaomi-classic-dev/android_device_xiaomi_msm8974-common

system_prop.mk
@@ -126,7 +126,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.latch_unsignaled=1 \
-    debug.sf.disable_backpressure=1
+    debug.sf.disable_backpressure=1 \
+    debug.sf.enable_gl_backpressure=1
 
 # Maintainer
 PRODUCT_PROPERTY_OVERRIDES += \