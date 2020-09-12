bc817de830da6b176f7ef3ca010d28cdae4c8910
Arian <arian.kulmer@web.de>
2020-09-12 12:33:48 +0200

    msm8974-common: Allow building with copy headers for now
    Change-Id: Ic2ac8483e723a5180d4a34c9b8e85f72654f8875
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat80@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+BUILD_BROKEN_USES_BUILD_COPY_HEADERS := true
+
 BOARD_VENDOR := xiaomi
 
 PLATFORM_PATH := device/xiaomi/msm8974-common
