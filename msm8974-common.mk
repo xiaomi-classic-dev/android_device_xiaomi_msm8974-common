fe3c37e42b22dc07d2517e4117cf771e8949992c
Arne Coucheron <arco68@gmail.com>
2020-10-29 09:28:12 +0100

    msm8974-common: Move android.hardware.audio to hwbinder
    Change-Id: I63be890030e9d2d3c5f2ca35ecd8142e6fa92d46

--- msm8974-common.mk
+++ msm8974-common.mk
@@ -63,6 +63,7 @@ PRODUCT_COPY_FILES += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
+    android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.broadcastradio@1.0-impl \
     android.hardware.soundtrigger@2.0-impl \
