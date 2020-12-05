e4c742e71467d4df6f70196d318a124327fa38fc
Bruno Martins <bgcngm@gmail.com>
2020-12-05 12:50:11 +0000

    msm8974-common: Kill legacy keystore and binderize keymaster HAL
    Legacy credential hash support was obsolete and got removed completely.
    From now on all the devices need to rely on gatekeeper, which
    in case of legacy devices should be the software implementation.
    
    This fixes setting a lockscreen PIN/password/pattern.
    
    Ref: https://android.googlesource.com/platform/frameworks/base/+/a758727c963948ae07e97c977a63b882216e8971
    
    Change-Id: I020d1807db13b73f9990ac3704eadba49b65ee5c

--- msm8974-common.mk
+++ msm8974-common.mk
@@ -164,7 +164,7 @@ PRODUCT_COPY_FILES += \
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
-    keystore.msm8974
+    android.hardware.keymaster@3.0-service
 
 # Lights
 PRODUCT_PACKAGES += \
